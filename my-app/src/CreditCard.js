81c2258d1bc1cc07567a91a3f3fe988de4dc46bf
PolkaDott <andrey.gurt@yandex.ru>
2021-05-22 12:33:50 +0500

    changes

--- my-app/src/CreditCard.js
+++ my-app/src/CreditCard.js
@@ -3,28 +3,28 @@ import React, {Component} from "react";
 class CreditCard extends Component {
   render() {
     return (
-      <div class="container">
-      <div class="row">
-       <div class="col-3"></div>
-       <div class="creditCardForm col-6">
-       <div class="heading">
+      <div className="container">
+      <div className="row">
+       <div className="col-3"></div>
+       <div className="creditCardForm col-6">
+       <div className="heading">
        <h1>Input parent's <b>credit</b> card to get access our amazing service!</h1>
        </div>
-       <div class="payment">
+       <div className="payment">
        <form>
-       <div class="form-group owner">
-       <label for="owner">Owner</label>
-       <input type="text" class="form-control" id="owner"/>
+       <div className="form-group owner">
+       <label htmlFor="owner">Owner</label>
+       <input type="text" className="form-control" id="owner"/>
        </div>
-       <div class="form-group CVV">
-       <label for="cvv">CVV</label>
-       <input type="text" class="form-control" id="cvv"/>
+       <div className="form-group CVV">
+       <label htmlFor="cvv">CVV</label>
+       <input type="text" className="form-control" id="cvv"/>
        </div>
-       <div class="form-group" id="card-number-field">
-       <label for="cardNumber">Card Number</label>
-       <input type="text" class="form-control" id="cardNumber"/>
+       <div className="form-group" id="card-number-field">
+       <label htmlFor="cardNumber">Card Number</label>
+       <input type="text" className="form-control" id="cardNumber"/>
        </div>
-       <div class="form-group" id="expiration-date">
+       <div className="form-group" id="expiration-date">
        <label>Expiration Date</label>
        <select>
        <option value="01">January</option>
@@ -49,19 +49,19 @@ class CreditCard extends Component {
        <option value="21"> 2021</option>
        </select>
        </div>
-       <div class="form-group" id="credit_cards">
+       <div className="form-group" id="credit_cards">
        <img src="https://bootstraptema.ru/snippets/form/2017/visa.jpg" id="visa"/>
        <img src="https://bootstraptema.ru/snippets/form/2017/mastercard.jpg" id="mastercard"/>
        <img src="https://bootstraptema.ru/snippets/form/2017/amex.jpg" id="amex"/>
        </div>
-       <div class="form-group" id="pay-now">
-       <button type="submit" class="btn btn-default" id="confirm-purchase">Confirm</button>
+       <div className="form-group" id="pay-now">
+       <button type="submit" className="btn btn-default" id="confirm-purchase">Confirm</button>
        </div>
        </form>
        </div>
        </div>
 
-       <div class="col-3"></div>
+       <div className="col-3"></div>
       </div>
       </div>
     );
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -7,23 +7,23 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-sm sticky-top">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-          <div className="container-fluid">
-            <a href="#" className="navbar-brad">Smart Scheduler</a>
-            <div id="navbarResponsive">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div className="container-fluid text-end">
+            <a className="navbar-brand" href="#">Smart Scheduler</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to='/creditcard' className="nav-link">Log in</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/registration' className="nav-link">Log up</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/logout' className="nav-link">Log out</Link>
-                </li>
+                  <li className="nav-item">
+                    <Link to='/creditcard' className="nav-link">Log in</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/registration' className="nav-link">Log up</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/logout' className="nav-link">Log out</Link>
+                  </li>
               </ul>
             </div>
           </div>
